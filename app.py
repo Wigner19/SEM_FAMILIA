1ff2de3e6e65afd323013aebcc09c2e7b72efe78
Wigner19 <wigner19@gmail.com>
2021-09-19 11:33:22 -0300

    Iniciar app do pacote Teams

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-import Users
+import Users, Teams
 from Database.db import mongo
 
 
@@ -10,6 +10,7 @@ def create_app(config_object="config.settings"):
     app = Flask(__name__)
     app.config.from_object(config_object)
     Users.init_app(app)
+    Teams.init_app(app)
     mongo.init_app(app)
 
     return app
